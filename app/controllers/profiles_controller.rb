e442c115228a69b43adcfc57dec6d65072b5aaf4
Robert Buchberger <rbuchberger@gmail.com>
2017-11-29 21:42:24 +0000

    added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,6 +22,12 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
+  # GET request to /users/:user_id/profile/edit
+  def edit
+    @user = User.find( params[:user_id])
+    @profile = @user.profile
+    
+  end
   
   private
     def profile_params
